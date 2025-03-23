b85571661937832dee42a4fd282b5356f93103d5
Blacknova7 <87141874+iBlacknova@users.noreply.github.com>
2025-03-23 16:41:48 +0000

    updates the styling of the contact form

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -1,37 +1,90 @@
 function Contact() {
   return (
-    <div className="mx-auto flex max-w-md flex-col items-center gap-y-6 rounded-xl bg-white p-6 shadow-lg outline outline-black/5">
-      <form className="flex w-full flex-col gap-y-4">
-        <label htmlFor="cname">Name:</label>
-        <input
-          type="text"
-          id="cname"
-          name="cname"
-          className="rounded border border-gray-300 p-2"
-        />
-
-        <label htmlFor="cemail">Email:</label>
-        <input
-          type="email"
-          id="cemail"
-          name="cemail"
-          className="rounded border border-gray-300 p-2"
-        />
-
-        <label htmlFor="cmessage">Message:</label>
-        <textarea
-          id="cmessage"
-          name="cmessage"
-          rows="4"
-          className="rounded border border-gray-300 p-2"
-        ></textarea>
-
-        <button
-          type="submit"
-          className="mt-4 rounded bg-black px-4 py-2 text-white hover:bg-blue-600"
-        >
-          Submit
-        </button>
+    <div className="isolate rounded-xl my-10 bg-white px-6 py-12 sm:py-16 lg:px-8">
+      <div className="mx-auto max-w-2xl text-center">
+        <h2 className="text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl">
+          Contact Me
+        </h2>
+      </div>
+      <form action="#" method="POST" className="mx-auto my-8 max-w-xl">
+        <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+          <div>
+            <label
+              htmlFor="first-name"
+              className="block text-sm/6 font-semibold text-gray-900"
+            >
+              First name
+            </label>
+            <div className="mt-2.5">
+              <input
+                id="first-name"
+                name="first-name"
+                type="text"
+                autoComplete="given-name"
+                className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="last-name"
+              className="block text-sm/6 font-semibold text-gray-900"
+            >
+              Last name
+            </label>
+            <div className="mt-2.5">
+              <input
+                id="last-name"
+                name="last-name"
+                type="text"
+                autoComplete="family-name"
+                className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+              />
+            </div>
+          </div>
+          <div className="sm:col-span-2">
+            <label
+              htmlFor="email"
+              className="block text-sm/6 font-semibold text-gray-900"
+            >
+              Email
+            </label>
+            <div className="mt-2.5">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+              />
+            </div>
+          </div>
+          <div className="sm:col-span-2">
+            <label
+              htmlFor="message"
+              className="block text-sm/6 font-semibold text-gray-900"
+            >
+              Message
+            </label>
+            <div className="mt-2.5">
+              <textarea
+                id="message"
+                name="message"
+                rows={4}
+                className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                defaultValue={""}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="mt-10">
+          <button
+            type="submit"
+            className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Let's talk
+          </button>
+        </div>
       </form>
     </div>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,9 @@
 import Header from "./Header";
-import Nav from "./Nav";
 
 function Home() {
   return (
     <>
       <Header />
-      <Nav />
     </>
   );
 }
